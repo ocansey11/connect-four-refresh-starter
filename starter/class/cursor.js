fe8acdf68e1c3697df4c8578b6fc172a8ccc1b21
kevin ocansey <kevinocansey11@gmail.com>
2023-02-04 11:56:26 +0000

    completed cursor.js

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -22,14 +22,49 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
-  left() {
-    // Move cursor left
-  }
 
-  right() {
+   right() {
     // Move cursor right
+    this.resetBackgroundColor()
+    if(this.col < this.numRows - 1){
+      this.col += 1
+    }
+    this.setBackgroundColor();
+    Screen.render();
   }
 
+  left() {
+      // Move cursor left
+      this.resetBackgroundColor()
+      if(this.col > 0){
+        this.col -= 1
+      }
+      this.setBackgroundColor();
+      Screen.render();
+    }
+
+
+  up() {
+    // Move cursor up
+    this.resetBackgroundColor()
+    if(this.row > 0){
+      this.row -= 1
+    }
+
+    this.setBackgroundColor();
+    Screen.render();
+  }
+
+  down() {
+    // Move cursor down
+    this.resetBackgroundColor()
+    if(this.row < this.numRows - 1){
+      this.row += 1
+    }
+
+    this.setBackgroundColor();
+    Screen.render();
+  }
 }
 
 
